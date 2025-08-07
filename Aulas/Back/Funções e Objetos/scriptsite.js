3e2ff61f67f5622cf09197a48a2ba7c31df8c90c
Diego Pereira Gomes <diego.gomes.tec@despertar.org.br>
2025-08-07 11:12:44 -0300

    conclução do formulario

---
+++ Aulas/Back/Funções e Objetos/scriptsite.js
@@ -0,0 +1,28 @@
+// Usando JS no site usando class
+const form = document.querySelector(".formulario")
+const result = document.querySelector(".resultado")
+
+const pessoas = []
+
+form.addEventListener("submit", function(evento){
+
+    evento.preventDefault();
+
+    const nome = form.querySelector(".nome").value;
+    const sobrenome = form.querySelector(".sobrenome").value;
+    const peso = form.querySelector(".peso").value;
+    const altura = form.querySelector(".altura").value;
+
+    const pessoa = {
+        nome,
+        sobrenome,
+        peso,
+        altura
+    };
+
+    pessoas.push(pessoa);
+
+    console.log(pessoas);
+
+    result.innerHTML += `<p> ${nome} ${sobrenome} - kg:${peso} ${altura}<\p>`
+})
