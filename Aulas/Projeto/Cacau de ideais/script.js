bf0fcca94c7e5da3b901d9c4388144a76acf3179
DiegopgomesDEV <diego.gomes.tec@despertar.org.br>
2025-08-12 01:58:45 -0300

    mini alteração

--- Aulas/Projeto/Cacau de ideais/script.js
+++ Aulas/Projeto/Cacau de ideais/script.js
@@ -8,7 +8,7 @@ function botaoproduto() {
   const descricao = botaoCard.querySelector(".descricao").textContent;
   const valor = botaoCard.querySelector(".valor").textContent;
 
-  resultado.innerHTML += `<p> ${nomeProduto}, ${descricao}, ${valor}</p>`
+  resultado.innerHTML += `<p> ${nomeProduto}, ${descricao}, <strong>${valor}<strong/> </p>`
 }
 
 function botaoproduto2(){
@@ -16,7 +16,7 @@ function botaoproduto2(){
   const descricao2 = botaoCard2.querySelector(".descricao2").textContent;
   const valor2 = botaoCard2.querySelector(".valor2").textContent;
 
-  resultado.innerHTML += `<p> ${nomeProduto2}, ${descricao2}, ${valor2}</p>`
+  resultado.innerHTML += `<p> ${nomeProduto2}, ${descricao2}, <strong>${valor2}<strong/> </p>`
 }
 
   function botaoproduto3(){
@@ -24,5 +24,5 @@ function botaoproduto2(){
   const descricao3 = botaoCard3.querySelector(".descricao3").textContent;
   const valor3 = botaoCard3.querySelector(".valor3").textContent;
 
-  resultado.innerHTML += `<p> ${nomeProduto3}, ${descricao3}, ${valor3}</p>`
+  resultado.innerHTML += `<p> ${nomeProduto3}, ${descricao3}, <strong>${valor3}<strong/> </p>`
 }
