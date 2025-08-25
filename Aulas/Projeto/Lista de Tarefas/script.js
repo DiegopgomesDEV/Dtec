704fea8733563d57de3759a0917cc9a145603441
Diego Pereira Gomes <diego.gomes.tec@despertar.org.br>
2025-08-25 10:04:58 -0300

    lista de tarefas concluída

--- Aulas/Projeto/Lista de Tarefas/script.js
+++ Aulas/Projeto/Lista de Tarefas/script.js
@@ -1,26 +1,27 @@
+function adicionarTarefa(){
+    const input = document.getElementById('novaTarefa');
+    const texto = input.value.trim();
+    if(texto === "") return;
 
-const mostralista = document.getElementById("lista")
-const botaorem = document.querySelector('.remove')
-const input_inf = document.getElementById("inputusuario");
-const removetar = document.querySelector('.tarefas') 
+    const li = document.createElement("li")
+    const p = document.createElement("p")
+    p.textContent = texto;
+    p.className = "tarefa";
+    li.appendChild(p);
+    
+    const btnRemover = document.createElement("button");
+    btnRemover.textContent = "Remover";
+    btnRemover.className = "remove";
+    btnRemover.onclick = function() {
+        li.remove();
+    };
 
+    li.onclick = function(){
+        li.classList.toggle('feito');
+    }
 
-function addlista(){
-const cli = document.createElement("li")
-const saveinput = input_inf.value
-cli.textContent = (saveinput)
-cli.classList.toggle("tarefas")
-mostralista.appendChild(cli)
-const cbotao = document.createElement("button")
-cbotao.textContent = "Remove"
-cbotao.classList.toggle("remove")
-cli.appendChild(cbotao)
+    li.appendChild(btnRemover)
+    document.getElementById('lista').appendChild(li);
+
+    input.value = ""
 }
-
-
-    botaorem.addEventListener("click", function(){
-    removetar.remove()
-})
-
-
-
