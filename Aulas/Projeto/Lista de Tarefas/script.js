b9f860c9e53b4d77c635612902f7969c544b549f
DiegopgomesDEV <diego.gomes.tec@despertar.org.br>
2025-08-25 05:16:06 -0300

    commit

--- Aulas/Projeto/Lista de Tarefas/script.js
+++ Aulas/Projeto/Lista de Tarefas/script.js
@@ -1,9 +1,26 @@
-const input_inf = document.getElementById("inputusuario")
+
 const mostralista = document.getElementById("lista")
-const botaoadd = document.getElementsByClassName("adicionar")
-const criarli = document.createElement("li")
+const botaorem = document.querySelector('.remove')
+const input_inf = document.getElementById("inputusuario");
+const removetar = document.querySelector('.tarefas') 
+
 
 function addlista(){
-    
-    mostralista.innerHTML += `<li>ola</li> `
+const cli = document.createElement("li")
+const saveinput = input_inf.value
+cli.textContent = (saveinput)
+cli.classList.toggle("tarefas")
+mostralista.appendChild(cli)
+const cbotao = document.createElement("button")
+cbotao.textContent = "Remove"
+cbotao.classList.toggle("remove")
+cli.appendChild(cbotao)
 }
+
+
+    botaorem.addEventListener("click", function(){
+    removetar.remove()
+})
+
+
+
