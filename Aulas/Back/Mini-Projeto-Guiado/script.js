2ee31318d899770fac731664a64ec95830dcfedf
Diego Pereira Gomes <diego.gomes.tec@despertar.org.br>
2025-09-05 11:29:15 -0300

    Update MINI-PJ Concluído

--- Aulas/Back/Mini-Projeto-Guiado/script.js
+++ Aulas/Back/Mini-Projeto-Guiado/script.js
@@ -20,6 +20,14 @@ const buscarBtn = document.getElementById("buscarBtn")
 //    console.log(país.name.common)
 //})
 
+// Dar função a teclas do teclado no site 
+document.addEventListener('keydown', (event) => {
+    if(event.key === "Enter"){
+        event.preventDefault()
+        buscarBtn.click()
+    }
+})
+
 buscarBtn.addEventListener('click', () => {
     const nomePaís = paísInput.value.trim();
 
@@ -29,20 +37,38 @@ buscarBtn.addEventListener('click', () => {
         return;
     }
 
-    const url = `https://restcountries.com/v3.1/name/${nomePaís}`
+    const url = `https://restcountries.com/v3.1/translation/${nomePaís}`
+
+    container.innerHTML = "<p>Busacando...</p>"
 
     fetch(url)
-        .then(response => {
+    .then(response => {
+        if(!response.ok){
+                throw new Error("País Não Encontrado")
+        }
+
         return response.json();
     })
 
     .then(data => {
         const país = data[0]
+        const moeda = Object.values(país.currencies)[0].name;
 
         container.innerHTML = `
-            <h2>${país.name.common}</h2>
-            <img src="${país.flags.svg}" alt= "Bandeira ${país.name.common}" width="150">
+            <h2>${país.translations.por.common}</h2>
+            <img src="${país.flags.svg}" alt= "Bandeira ${país.translations.por}" width="150">
+            <p><strong>Capital:</strong> ${país.capital[0]}</p>
+            <p><strong>População:</strong> ${país.population.toLocaleString()}</p>
+            <p><strong>Moeda:</strong> ${moeda}</p>
+            `
 
-        `
+            paísInput.value = ""
+            
+    })
+
+    .catch(error => {
+        console.error(error)
+        container.innerHTML = `<p style= "color: red;">Erro: ${error.message}</p>`
+        paísInput.value = ""
     })
 })
