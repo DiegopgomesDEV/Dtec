ac9fbafc315839e5d68eea9ac152456d6ce637c8
Diego Pereira Gomes <diego.gomes.tec@despertar.org.br>
2025-09-10 11:28:38 -0300

    Update API e Consulta Clima

--- Aulas/Back/Minha_API/server.js
+++ Aulas/Back/Minha_API/server.js
@@ -13,11 +13,39 @@ const PORT = 3001;
 let usuarios = [
     {id: 1, nome: "Ana", idade: 20},
     {id: 2, nome: "Carlos", idade: 30},
-    {id: 3, nome: "Maria", idade: 22}
+    {id: 3, nome: "Maria", idade: 22},
+    {id: 4, nome: "Maria Juana", idade: 22},
+    {id: 5, nome: "Carlos Eduardo", idade: 22}
 ]
 
-app.get(`/usuarios`,(req,res) => {
+app.get(`/`,(req, res) => {
+    res.send("PÁGINA INICIAL")
+})
+
+app.get(`/usuarios`,(req, res) => {
     res.json(usuarios);
+})
+
+app.get('/usuarios/:id', (req, res) => {
+    const id = req.params.id
+    const usuario = usuarios.find(u => u.id == id)
+
+    if(usuario){
+        res.json(usuario)
+    }else{
+        res.status(404).json({mensagem:"Usuário Não Encontrado"})
+    }
+})
+
+app.get('/usuarios/buscar/:nome', (req, res) => {
+    const buscaNome = req.params.nome.toLocaleLowerCase()
+    const resultados = usuarios.filter(u => u.nome.toLocaleLowerCase().includes(buscaNome))
+
+    if(resultados.length > 0){
+        res.json(resultados)
+    }else{
+        res.status(404).json({mensagem:"Usuário Não Encontrado"})
+    }
 })
 
 //Inicia o Servidor
